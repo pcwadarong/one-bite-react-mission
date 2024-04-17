74fa8705bbe4fd0acb6ababa3d72cc2f85cbec0a
chaen <cw06155@gmail.com>
2024-04-17 20:21:07 +0900

    Day 16

---
+++ Day 16/src/App.jsx
@@ -0,0 +1,55 @@
+import './App.css';
+import ContactEditor from './components/ContactEditor';
+import ContactList from './components/ContactList';
+import { useRef, useReducer, useEffect } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CREATE':
+      return [action.data, ...state];
+    case 'DELETE':
+      return state.filter((item) => item.id !== action.id);
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [contacts, dispatch] = useReducer(reducer, []);
+  const isRef = useRef(0);
+  useEffect(() => {
+    console.log('update', contacts);
+  }, [contacts]);
+
+  const onCreate = (name, contact) => {
+    dispatch({
+      type: 'CREATE',
+      data: {
+        id: isRef.current++,
+        name,
+        contact,
+      },
+    });
+  };
+
+  const onDelete = (id) => {
+    dispatch({
+      type: 'DELETE',
+      id,
+    });
+  };
+
+  return (
+    <div className="App">
+      <h2>Contact List</h2>
+      <section>
+        <ContactEditor onCreate={onCreate} />
+      </section>
+      <section>
+        <ContactList contacts={contacts} onDelete={onDelete} />
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ Day 16/src/components/ContactItem.jsx
@@ -0,0 +1,11 @@
+import './ContactItem.css';
+
+export default function ContactItem({id, name, contact, onDelete}) {
+  return (
+    <div className="ContactItem">
+      <div className="name">{name}</div>
+      <div className="contact">{contact}</div>
+      <button onClick={() => onDelete(id)}>🗑️ Remove</button>
+    </div>
+  );
+}
