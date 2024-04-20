32a5f0ba5fd2d6526fdd47b76083a73e7adffa90
chaen <cw06155@gmail.com>
2024-04-21 03:39:25 +0900

    install tailwind + make common css style

--- Day 20/src/App.jsx
+++ Day 20/src/App.jsx
@@ -5,12 +5,7 @@ import New from './pages/New';
 import Home from './pages/Home';
 import Notfound from './pages/Notfound';
 import Edit from './pages/Edit';
-
-import emotion1 from './assets/emotion1.png';
-import emotion2 from './assets/emotion2.png';
-import emotion3 from './assets/emotion3.png';
-import emotion4 from './assets/emotion4.png';
-import emotion5 from './assets/emotion5.png';
+import { getEmotionImage } from './utils/get-emotion-image';
 
 function App() {
   const nav = useNavigate();
@@ -21,13 +16,13 @@ function App() {
   return (
     <div>
       <div>
-        <img src={emotion1} alt="emotion1" />
-        <img src={emotion5} alt="emotion5" />
+        <img src={getEmotionImage(1)} alt="emotion1" />
+        <img src={getEmotionImage(5)} alt="emotion5" />
       </div>
       <Link to={'/'}> Home </Link>
       <Link to={'/New'}> New </Link>
       <Link to={'/Diary'}> Diary </Link>
-      {/* <Link to={'/Edit'}> Edit </Link> */}
+      <Link to={'/Edit'}> Edit </Link>
 
       <button onClick={onClickBtn}>btn to Edit</button>
       <Routes>
